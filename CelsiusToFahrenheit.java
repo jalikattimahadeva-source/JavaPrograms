e233aa6a13b5608cc7573d4753188465965d8c1b
jalikattimahadeva-source <jalikattimahadeva@gmail.com>
2026-02-18 15:34:34 +0530

    Add files via upload

---
+++ CelsiusToFahrenheit.java
@@ -0,0 +1,13 @@
+//import java.util.*;
+class CelsiusToFahrenheit{
+    public static void main(String args[])
+    {
+        double celsius=45;
+        double farhenheit;
+        //Scanner sc = new Scanner(System.in);
+        System.out .println("enter the temperature of the celsius:");
+        //celsius=sc.nextInt();
+        farhenheit=(celsius*9/5)+32;
+        System.out.println("the temperature in farhenheit is:"+farhenheit);
+    }
+}
